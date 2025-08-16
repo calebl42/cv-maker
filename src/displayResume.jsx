6a2044709a71cc96562352f8177faef716eea298
CalebL42 <caleb_lee@ucsb.edu>
2025-08-16 04:05:34 -0700

    enlarged pdf viewer and got download resume button working

--- src/displayResume.jsx
+++ src/displayResume.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { useMemo } from 'react'
 import * as pdf from '@react-pdf/renderer'
 
+function ResumePDF() {
+  return (
+    <pdf.Document>
+        <pdf.Page>
+          <pdf.Text>buns</pdf.Text>
+        </pdf.Page>
+    </pdf.Document>
+  )
+}
 function DisplayResume({resume, setResume, toggleDisplayMode}) {
   return (
     <div id='display-resume'>
+      <pdf.PDFDownloadLink document={<ResumePDF />} fileName='resume.pdf'>
+        {({ loading }) =>
+          loading ? 'preparing your resume...' : <button>download resume</button>
+        }
+      </pdf.PDFDownloadLink>
       <button onClick={toggleDisplayMode}>edit resume</button>
       <pdf.PDFViewer id='pdf-viewer'>
-        <pdf.Document>
-          <pdf.Page>
-            
-          </pdf.Page>
-        </pdf.Document>
+        <ResumePDF />
       </pdf.PDFViewer>
     </div>
   )
